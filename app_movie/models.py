ea17b65421af28bb777f6417add5fffe8bbef9aa
rahmatnazali95 <rahmatnazali95@gmail.com>
2020-06-03 06:43:00 +0700

    make relation null safe

--- app_movie/models.py
+++ app_movie/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.utils.text import slugify
 import uuid
 # Create your models here.
+LANGUAGE = (
+        ('EN', 'English'),
+        ('KR', 'Korean'),
+        ('HI', 'Hindi'),
+        ('TM', 'Tamil'),
+        ('MD', 'Mandarin'),
+        # and so on ..
+        ('-', 'N/A'),
+)
 
 class Rating(models.Model):
     name = models.CharField(max_length=10, unique=True)  # make it unique and indexed for filter/search
@@ -14,18 +23,11 @@ class Movie(models.Model):
     description = models.TextField()
     thumbnail = models.URLField()
     duration = models.IntegerField()
-    language = models.CharField(max_length=2, choices=(
-        ('EN', 'English'),
-        ('KR', 'Korean'),
-        ('HI', 'Hindi'),
-        ('TM', 'Tamil'),
-        ('MD', 'Mandarin'),
-        # and so on ..
-    ), db_index=True)  # could be indexed for filtering by language
+    language = models.CharField(max_length=2, choices=LANGUAGE, db_index=True)  # could be indexed for filtering by language
     user_rating = models.FloatField()
 
     genre = models.ManyToManyField(Genre, related_name="movies_in_genre")
-    rating = models.ForeignKey(Rating, on_delete=models.DO_NOTHING, related_name="movies_in_rating")
+    rating = models.ForeignKey(Rating, on_delete=models.DO_NOTHING, related_name="movies_in_rating", null=True, blank=True)
     rating_label = models.CharField(max_length=100)  # I put the rating label here, because movies withthe same MPAA Rating could have different label
 
     slug = models.SlugField(unique=True) # make it unique and indexed for url hit
